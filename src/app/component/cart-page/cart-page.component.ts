a59b9ca5ca0623598a5a9557d4a680e68ee3ee4d
Muhammed Rafeeq <rafeeqktt918@gmail.com>
2023-12-27 09:27:25 +0530

    Initial commit

---
+++ src/app/component/cart-page/cart-page.component.ts
@@ -0,0 +1,33 @@
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/api.service';
+import { product } from '../productmodel';
+
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit{
+  showproduct:any=[];
+  public addressform=false;
+  public totalamount :number=0;
+  constructor(private api:ApiService){}
+  ngOnInit(): void {
+    this.api.products().subscribe(res=>{
+      this.showproduct = res;
+      this.totalamount = this.api.calculateprice();
+      console.log("total amt is",this.totalamount)
+    })
+  }
+  deleteitem(item:product){
+   this.api.removecartitem(item);
+  }
+  Empty(){
+    this.api.removeallitems()
+  }
+  cancel(){
+    this.addressform = false;
+  }
+
+}
